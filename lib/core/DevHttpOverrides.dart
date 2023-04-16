eebe67898ab624a9331234a8aa23dbf8e600fc3d
Sergey Ivannikov <ivannikov.s@gmail.com>
2023-04-16 13:07:32 +0300

    added DevHttpOverrides

--- lib/core/DevHttpOverrides.dart
+++ lib/core/DevHttpOverrides.dart
@@ -2,8 +2,18 @@
 import 'dart:io';
 
 class DevHttpOverrides extends HttpOverrides {
+
   @override
   HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)
+      {
+        if (host.isNotEmpty && host == 'localhost')
+        {
+          return true;
+        }
+        else
+        {  return false;  }
+      };
   }
 }
